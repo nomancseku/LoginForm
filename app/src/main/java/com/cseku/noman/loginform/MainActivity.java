1ca974f71889b0b8ef4af8c1049e4ff0eed30d25
nomancseku <nomancseku@gmail.com>
2018-08-21 16:23:53 +0600

    2nd commit

--- app/src/main/java/com/cseku/noman/loginform/MainActivity.java
+++ app/src/main/java/com/cseku/noman/loginform/MainActivity.java
@@ -2,6 +2,7 @@ package com.cseku.noman.loginform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private TextView Info;
     private Button Login;
+    private int counter = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +25,34 @@ public class MainActivity extends AppCompatActivity {
         Password = (EditText)findViewById(R.id.etPassword);
         Info = (TextView)findViewById(R.id.tvInfo);
         Login = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("Number of attempts remaining : 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
     }
 
     private void validate(String userName, String userPassword)
     {
-        if((userName=="admin") && (userPassword == "1234"))
+        if((userName.equals("admin")) && (userPassword.equals("1234")))
         {
             Intent intent = new Intent(MainActivity.this,SecondActivity.class);
             // Intent(source,destination)
+            startActivity(intent);
+        }
+        else
+        {
+            counter--;
+            Info.setText("Number of attempts remaining : " + String.valueOf(counter));
+
+            if(counter==0)
+            {
+                Login.setEnabled(false);
+            }
         }
     }
 }
